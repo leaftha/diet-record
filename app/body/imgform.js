b0c394ddbc0f94004b28a939cb4ac5cf63fc6db4
SaChan <vltpcks@gmail.com>
2023-07-06 23:38:59 +0900

    img empty upload

--- app/body/imgform.js
+++ app/body/imgform.js
@@ -1,98 +1,110 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Imgform({ session, year, month }) {
-    const [resimag, setResImg] = useState({});
-    const [imagefile, setImagefile] = useState({});
-    const [imgSrc, setImgSrc] = useState();
+  const [resimag, setResImg] = useState({});
+  const [isImg, setIsImg] = useState(false);
+  const [imagefile, setImagefile] = useState({});
+  const [imgSrc, setImgSrc] = useState();
 
-    const [resimag2, setResImg2] = useState({});
-    const [imagefile2, setImagefile2] = useState({});
-    const [imgSrc2, setImgSrc2] = useState();
-    const [forntname, setFrontName] = useState('');
-    const [sidename, setSideName] = useState('');
+  const [resimag2, setResImg2] = useState({});
+  const [imagefile2, setImagefile2] = useState({});
+  const [isImg2, setIsImg2] = useState(false);
 
-    async function submit(e) {
-        //S3 업로드
-        let file = imagefile;
-        const formData = new FormData();
-        Object.entries({ ...resimag.fields, file }).forEach(([key, value]) => {
-            formData.append(key, value);
-        });
-        let result = await fetch(resimag.url, {
-            method: 'POST',
-            body: formData,
-        });
+  const [imgSrc2, setImgSrc2] = useState();
+  const [forntname, setFrontName] = useState("");
+  const [sidename, setSideName] = useState("");
 
-        file = imagefile2;
-        const formData2 = new FormData();
-        Object.entries({ ...resimag2.fields, file }).forEach(([key, value]) => {
-            formData2.append(key, value);
-        });
-        let result2 = await fetch(resimag2.url, {
-            method: 'POST',
-            body: formData2,
-        });
+  async function submit(e) {
+    if (isImg && isImg2) {
+      let file = imagefile;
+      const formData = new FormData();
+      Object.entries({ ...resimag.fields, file }).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+      let result = await fetch(resimag.url, {
+        method: "POST",
+        body: formData,
+      });
+
+      file = imagefile2;
+      const formData2 = new FormData();
+      Object.entries({ ...resimag2.fields, file }).forEach(([key, value]) => {
+        formData2.append(key, value);
+      });
+      let result2 = await fetch(resimag2.url, {
+        method: "POST",
+        body: formData2,
+      });
+    } else {
+      alert("이미지를 두개 모두 선택해주십시요");
     }
-    return (
+  }
+  return (
+    <div>
+      <form onSubmit={submit}>
         <div>
-            <form onSubmit={submit}>
-                <div>
-                    <lable>front</lable>
-                    <input
-                        type="file"
-                        name="front"
-                        accept="image/*"
-                        onChange={async (e) => {
-                            let file = e.target.files[0];
-                            setImagefile(file);
-                            let image = window.URL.createObjectURL(file);
-                            setImgSrc(image);
-                            let filename = encodeURIComponent(file.name);
-                            setFrontName(filename);
-                            let res = await fetch('/api/post/image?file=' + filename);
-                            res = await res.json();
-                            setResImg(res);
-                        }}
-                    />
-                    <lable>Side</lable>
-                    <input
-                        type="file"
-                        name="side"
-                        accept="image/*"
-                        onChange={async (e) => {
-                            let file = e.target.files[0];
-                            setImagefile2(file);
-                            let image = window.URL.createObjectURL(file);
-                            setImgSrc2(image);
-                            let filename = encodeURIComponent(file.name);
-                            setSideName(filename);
-                            let res = await fetch('/api/post/image?file=' + filename);
-                            res = await res.json();
-                            setResImg2(res);
-                        }}
-                    />
-                </div>
-                <button
-                    onClick={() => {
-                        fetch('api/post/imgupload', {
-                            method: 'POST',
-                            body: JSON.stringify({
-                                email: `${session.user.email}`,
-                                year: `${year}`,
-                                month: `${month}`,
-                                linkfront: `https://diet-record-images.s3.ap-northeast-2.amazonaws.com/${forntname}`,
-                                linkside: `https://diet-record-images.s3.ap-northeast-2.amazonaws.com/${sidename}`,
-                            }),
-                        });
-                    }}
-                >
-                    입력
-                </button>
-            </form>
-            <img src={imgSrc}></img>
-            <img src={imgSrc2}></img>
+          <lable>front</lable>
+          <input
+            type="file"
+            name="front"
+            accept="image/*"
+            onChange={async (e) => {
+              let file = e.target.files[0];
+              setImagefile(file);
+              setIsImg(true);
+              let image = window.URL.createObjectURL(file);
+              setImgSrc(image);
+              let filename = encodeURIComponent(file.name);
+              setFrontName(filename);
+              let res = await fetch("/api/post/image?file=" + filename);
+              res = await res.json();
+              setResImg(res);
+            }}
+          />
+          <lable>Side</lable>
+          <input
+            type="file"
+            name="side"
+            accept="image/*"
+            onChange={async (e) => {
+              let file = e.target.files[0];
+              setImagefile2(file);
+              setIsImg2(true);
+              let image = window.URL.createObjectURL(file);
+              setImgSrc2(image);
+              let filename = encodeURIComponent(file.name);
+              setSideName(filename);
+              let res = await fetch("/api/post/image?file=" + filename);
+              res = await res.json();
+              setResImg2(res);
+            }}
+          />
         </div>
-    );
+        <button
+          onClick={() => {
+            if (isImg && isImg2) {
+              fetch("api/post/imgupload", {
+                method: "POST",
+                body: JSON.stringify({
+                  email: `${session.user.email}`,
+                  year: `${year}`,
+                  month: `${month}`,
+                  linkfront: `https://diet-record-images.s3.ap-northeast-2.amazonaws.com/${forntname}`,
+                  linkside: `https://diet-record-images.s3.ap-northeast-2.amazonaws.com/${sidename}`,
+                }),
+              });
+            } else {
+              alert("이미지를 두개 모두 선택해주십시요");
+            }
+          }}
+        >
+          입력
+        </button>
+      </form>
+      <img src={imgSrc}></img>
+      <img src={imgSrc2}></img>
+    </div>
+  );
 }
--- app/body/page.js
+++ app/body/page.js
@@ -1,33 +1,32 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth].js';
-import { getServerSession } from 'next-auth';
-import { connectDB } from '@/util/database';
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import { connectDB } from "@/util/database";
 
-import Imgform from './imgform';
-import NotAuth from '../notauth';
-import Imgslide from './imgslide';
+import Imgform from "./imgform";
+import NotAuth from "../notauth";
+import Imgslide from "./imgslide";
 
 export default async function Body() {
-    let session = await getServerSession(authOptions);
-    if (session === null) {
-        return NotAuth();
-    }
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
+  let session = await getServerSession(authOptions);
+  if (session === null) {
+    return NotAuth();
+  }
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
 
-    const client = await connectDB;
-    const db = client.db('menber');
+  const client = await connectDB;
+  const db = client.db("menber");
 
-    let result = await db.collection('front').findOne({
-        year: `${year}`,
-        month: `${month}`,
-    });
-    console.log(result);
-    return (
-        <div>
-            {/* {result != null ? <div>이번달 입력완료</div> : <Imgform year={year} month={month} session={session} />} */}
-            <Imgform year={year} month={month} session={session} />
-            <Imgslide session={session} />
-        </div>
-    );
+  let result = await db.collection("front").findOne({
+    year: `${year}`,
+    month: `${month}`,
+  });
+  return (
+    <div>
+      {/* {result != null ? <div>이번달 입력완료</div> : <Imgform year={year} month={month} session={session} />} */}
+      <Imgform year={year} month={month} session={session} />
+      <Imgslide session={session} />
+    </div>
+  );
 }
