771ff1fa8f5c99a99eb0bc55732c251f2f67a0fa
leaftha <vltpcks@gmail.com>
2023-08-28 14:41:01 +0900

    profile page css

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,57 +1,58 @@
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
-import { connectDB } from "@/util/database";
-import NotAuth from "../notauth";
-import Link from "next/link";
-import classes from "./page.module.css";
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
+import { connectDB } from '@/util/database';
+import NotAuth from '../notauth';
+import Link from 'next/link';
+import classes from './page.module.css';
 // import classes from './page.module.css';
 
 export default async function InBody() {
-  let session = await getServerSession(authOptions);
-  if (session === null) {
-    return NotAuth();
-  }
-  const client = await connectDB;
-  const db = client.db("menber");
+    let session = await getServerSession(authOptions);
+    if (session === null) {
+        return NotAuth();
+    }
+    const client = await connectDB;
+    const db = client.db('menber');
 
-  let result = await db
-    .collection("inbody")
-    .find({ email: session.user.email })
-    .sort({ _id: -1 })
-    .limit(12)
-    .toArray();
+    let result = await db
+        .collection('inbody')
+        .find({ email: session.user.email })
+        .sort({ _id: -1 })
+        .limit(12)
+        .toArray();
 
-  result = result.map((a) => {
-    a._id = a._id.toString();
-    return a;
-  });
+    result = result.map((a) => {
+        a._id = a._id.toString();
+        return a;
+    });
 
-  const currentWeight = result[0].weight;
-  const currentInbody = result[0].fatper;
-  const goalWeight = session.user.weight;
-  const goalInbody = session.user.inbody;
-  return (
-    <div className={classes.container}>
-      <div className={classes.main}>
-        <div className={classes.item}>
-          <p>{session.user.name}</p>
-          <p>{session.user.email}</p>
+    const currentWeight = result[0].weight;
+    const currentInbody = result[0].fatper;
+    const goalWeight = session.user.weight;
+    const goalInbody = session.user.inbody;
+    return (
+        <div className={classes.container}>
+            <div className={classes.main}>
+                <div className={classes.item}>
+                    <p>{session.user.name}</p>
+                    <p>{session.user.email}</p>
+                </div>
 
-          <div className={classes.item}>
-            <Link href="profileSetup">목표 체중 설정</Link>
-          </div>
+                <div className={classes.item}>
+                    <p>목표 체중 : {goalWeight}</p>
+                    <p>목표 체지방률 : {goalInbody}%</p>
+                </div>
+
+                <div className={classes.item}>
+                    <p>현재 체중 : {currentWeight}</p>
+                    <p>현재 체지방률 : {currentInbody}</p>
+                </div>
+            </div>
+            <div className={classes.change}>
+                <Link className={classes.link} href="profileSetup">
+                    목표 체중 설정
+                </Link>
+            </div>
         </div>
-
-        <div className={classes.item}>
-          <p>현재 체중 : {currentWeight}</p>
-          <p>현재 체지방률 : {currentInbody}</p>
-        </div>
-
-        <div className={classes.item}>
-          <p>목표 체중 : {goalWeight}</p>
-          <p>목표 체지방률 : {goalInbody}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
