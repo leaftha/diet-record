4d8abee9de4a7772fe3487524fe1593bf7ce5abc
leaftha <vltpcks@gmail.com>
2023-08-29 13:46:26 +0900

    code cleanup

--- pages/api/auth/withdrawal.js
+++ pages/api/auth/withdrawal.js
@@ -1,17 +1,12 @@
 import { connectDB } from '@/util/database';
-import bcrypt from 'bcrypt';
 
 export default async function handler(Get, Post) {
     if (Get.method == 'POST') {
         const client = await connectDB;
         const db = client.db('menber');
-        // let foundEmail = await db.collection('user_cred').findOne({ email: Get.body.email });
-        // let foundInbody = await db.collection('daliy').findOne({ email: Get.body.email });
-        // let foundDaily = await db.collection('inbody').findOne({ email: Get.body.email });
         db.collection('user_cred').deleteMany({ email: Get.body.email });
         db.collection('daliy').deleteMany({ email: Get.body.email });
         db.collection('inbody').deleteMany({ email: Get.body.email });
         Post.status(200).redirect(302, '/');
-        console.log(foundEmail, foundInbody, foundDaily);
     }
 }
