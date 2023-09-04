6bf5d48a325b90f31f48a393f7d27af716de9ec1
leaftha <vltpcks@gmail.com>
2023-09-04 15:15:04 +0900

    add arrow

--- app/page.js
+++ app/page.js
@@ -15,10 +15,17 @@ export default function Home() {
                     <li>여기서 당신의 도전이 성공하길!</li>
                 </ul>
             </div>
-            <div className={classes.side}>d</div>
+            <div className={classes.side}>
+                <div className={classes.arrow}>{`↓`}</div>
+            </div>
             <div className={classes.item}>
-                <p>인바디 페이지에서는 자신의 체중, 근육량, 체지방을 입력하고 월 단위로 그래프 비교가 가능합니다.</p>
-                <p>목표 체중을 입력했다면 목표체중까지 얼마나 남았는지 알수 있습니다.</p>
+                <div>
+                    <p>
+                        인바디 페이지에서는 자신의 체중, 근육량, 체지방을 입력하고
+                        <br /> 월 단위로 그래프 비교가 가능합니다.
+                    </p>
+                    <p>목표 체중을 입력했다면 목표체중까지 얼마나 남았는지 알수 있습니다.</p>
+                </div>
             </div>
             <div className={classes.imgItem}>
                 <img src="/images/inbody.png" alt="inbody img"></img>
@@ -27,14 +34,24 @@ export default function Home() {
                 <img src="/images/daily.png" alt="daily img"></img>
             </div>
             <div className={classes.item}>
-                <p>운동기록 페이지에서는 자신의 운동을 기록하고 한달에 얼마나 운동했는지 알수 있습니다.</p>
-                <p>청록색으로 표시되는 오늘의 날짜를 클릭하여 나타나는 모달창에 오늘 한 운동을 기록하세요</p>
+                <div>
+                    <p>
+                        운동기록 페이지에서는 자신의 운동을 기록하고
+                        <br /> 한달에 얼마나 운동했는지 알수 있습니다.
+                    </p>
+                    <p>
+                        청록색으로 표시되는 오늘의 날짜를 클릭하여
+                        <br /> 나타나는 모달창에 오늘 한 운동을 기록하세요
+                    </p>
+                </div>
             </div>
             <div className={classes.item}>
-                <p>
-                    프로필 페이지에서는 목표 체중을 입력가능하고 현재 체중과 자신의 이름 이메일을 알수 있으며 회원
-                    탈퇴도 가능합니다.
-                </p>
+                <div>
+                    <p>
+                        프로필 페이지에서는 목표 체중을 입력가능하고
+                        <br /> 현재 체중과 자신의 이름 이메일을 알수 있으며 회원 탈퇴도 가능합니다.
+                    </p>
+                </div>
             </div>
             <div className={classes.imgItem}>
                 <img src="/images/profile.png" alt="profile img"></img>
