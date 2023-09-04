3cdefc1a82cadb4a2186365a24f806aefecf7f1f
leaftha <vltpcks@gmail.com>
2023-09-04 15:26:30 +0900

    notauth page design

--- app/notauth.js
+++ app/notauth.js
@@ -1,7 +1,9 @@
+import classes from './notauth.module.css';
+
 export default function NotAuth() {
-  return (
-    <div>
-      <h1>로그인이 되어있지 않습니다.</h1>
-    </div>
-  );
+    return (
+        <div className={classes.main}>
+            <h1>로그인이 상태가 아닙니다.</h1>
+        </div>
+    );
 }
