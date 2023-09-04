7c34644474c122180d10224b82fb3f135be9f113
leaftha <vltpcks@gmail.com>
2023-09-04 14:24:13 +0900

    main page add sub

--- app/page.js
+++ app/page.js
@@ -1,21 +1,34 @@
-import classes from "./page.module.css";
+import classes from './page.module.css';
 
 export default function Home() {
-  return (
-    <div className={classes.content}>
-      <div className={classes.img}>
-        <h1 className={classes.title}>Diet</h1>
-        <img src="/images/main.jpg" alt="main img"></img>
-      </div>
-      <ul className={classes.subtitle}>
-        <li>다이어트를 위한 사이트입니다.</li>
-        <li>자신의 체중, 근육량, 체지방등을 입력하여 비교할수 있습니다.</li>
-        <li>
-          또한 하루 운동 기록을 입력하여 달력에 얼마나 운동했는지 알수 있습니다.
-        </li>
-        <li>여기서 당신의 도전이 성공하길!</li>
-      </ul>
-      <div className={classes.side}>d</div>
-    </div>
-  );
+    return (
+        <div className={classes.content}>
+            <div className={classes.img}>
+                <h1 className={classes.title}>Diet</h1>
+                <img src="/images/main.jpg" alt="main img"></img>
+            </div>
+            <div>
+                <ul className={classes.subtitle}>
+                    <li>다이어트를 위한 사이트입니다.</li>
+                    <li>자신의 체중, 근육량, 체지방등을 입력하여 비교할수 있습니다.</li>
+                    <li>또한 하루 운동 기록을 입력하여 달력에 얼마나 운동했는지 알수 있습니다.</li>
+                    <li>여기서 당신의 도전이 성공하길!</li>
+                </ul>
+            </div>
+            <div className={classes.side}>d</div>
+            <div className={classes.item}>
+                인바디 페이지에서는 자신의 체중, 근육량, 체지방을 입력하고 월 단위로 그래프 비교가 가능합니다.
+            </div>
+            <div>bb</div>
+            <div>aaa</div>
+            <div className={classes.item}>
+                운동기록 페이지에서는 자신의 운동을 기록하고 한달에 얼마나 운동했는지 알수 있습니다.
+            </div>
+            <div className={classes.item}>
+                프로필 페이지에서는 목표 체중을 입력가능하고 현재 체중과 자신의 이름 이메일을 알수 있으며 회원 탈퇴도
+                가능합니다.
+            </div>
+            <div>bb</div>
+        </div>
+    );
 }
