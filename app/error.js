01bcb59abfd66812362e3b44406c6f41935312dc
leaftha <vltpcks@gmail.com>
2023-09-04 15:29:54 +0900

    error page design

--- app/error.js
+++ app/error.js
@@ -1,9 +1,10 @@
-"use client";
+'use client';
+import classes from './error.module.css';
 
 export default function Error({ error, reset }) {
-  return (
-    <div>
-      <h4>에러입니다. 새로고침을 하거나 관리자에게 문의하세요</h4>
-    </div>
-  );
+    return (
+        <div className={classes.main}>
+            <h1>에러입니다. 새로고침을 하거나 관리자에게 문의하세요</h1>
+        </div>
+    );
 }
