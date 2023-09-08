7aaec99f045a449cc8d054cab5ba33176b6d84b3
leaftha <vltpcks@gmail.com>
2023-09-08 09:21:05 +0900

    register page mobile

--- app/register/page.js
+++ app/register/page.js
@@ -1,36 +1,26 @@
-import claases from "./page.module.css";
+import claases from './page.module.css';
 
 export default function Register() {
-  return (
-    <div className={claases.main}>
-      <form className={claases.form} method="POST" action="/api/auth/signup">
-        <label className={claases.label}>이름</label>
-        <input
-          className={claases.input}
-          name="name"
-          type="text"
-          placeholder="이름"
-        />
-        <label className={claases.label}>이메일</label>
-        <input
-          className={claases.input}
-          name="email"
-          type="text"
-          placeholder="이메일"
-        />
-        <label className={claases.label}>비밀번호</label>
-        <input
-          className={claases.input}
-          name="password"
-          type="password"
-          minLength={6}
-          maxLength={12}
-          placeholder="비번"
-        />
-        <button className={claases.btn} type="submit">
-          id/pw 가입요청
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <div className={claases.main}>
+            <form className={claases.form} method="POST" action="/api/auth/signup">
+                <label className={claases.label}>이름</label>
+                <input className={claases.input} name="name" type="text" placeholder="이름" />
+                <label className={claases.label}>이메일</label>
+                <input className={claases.input} name="email" type="text" placeholder="이메일" />
+                <label className={claases.label}>비밀번호</label>
+                <input
+                    className={claases.input}
+                    name="password"
+                    type="password"
+                    minLength={6}
+                    maxLength={12}
+                    placeholder="비번"
+                />
+                <button className={claases.btn} type="submit">
+                    회원가입
+                </button>
+            </form>
+        </div>
+    );
 }
