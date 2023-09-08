3b34abfa809de9add29cd163fd1679d998caf8b9
leaftha <vltpcks@gmail.com>
2023-09-08 09:10:22 +0900

    change

--- app/notauth.js
+++ app/notauth.js
@@ -3,7 +3,10 @@ import classes from './notauth.module.css';
 export default function NotAuth() {
     return (
         <div className={classes.main}>
-            <h1>로그인이 상태가 아닙니다.</h1>
+            <h1>
+                로그인 <br />
+                상태가 아닙니다.
+            </h1>
         </div>
     );
 }
