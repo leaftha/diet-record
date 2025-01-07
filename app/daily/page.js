9ff06ef71b585d0c5c86b7f3a2abac9ecde38d75
SaChan <vltpcks@gmail.com>
2025-01-07 21:12:01 +0900

    너무 확장된 컴포넌트 삭제

--- app/daily/page.js
+++ app/daily/page.js
@@ -1,45 +1,74 @@
-import Calender from './calender';
-import { connectDB } from '@/util/database';
-import { authOptions } from '@/pages/api/auth/[...nextauth].js';
-import { getServerSession } from 'next-auth';
-import NotAuth from '../notauth';
+import { connectDB } from "@/util/database";
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import CalenderCell from "./calenderCell";
+import classes from "./page.module.css";
+import NotAuth from "../notauth";
 
-import { startOfMonth, endOfMonth, startOfWeek, endOfWeek, format } from 'date-fns';
+import {
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  format,
+} from "date-fns";
 
 export default async function Daily() {
-    let session = await getServerSession(authOptions);
-    if (session === null) {
-        return NotAuth();
-    }
-    const currentMonth = new Date();
-    const monthStart = startOfMonth(currentMonth);
-    const monthEnd = endOfMonth(monthStart);
-    const startDate = startOfWeek(monthStart);
+  let session = await getServerSession(authOptions);
+  if (session === null) {
+    return NotAuth();
+  }
+  const currentMonth = new Date();
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const date = ["Sun", "Mon", "Thu", "Wed", "Thrs", "Fri", "Sat"];
 
-    const year = currentMonth.getFullYear();
-    const startMonth = format(startDate, 'M');
-    const endMonth = format(monthEnd, 'M');
+  const year = currentMonth.getFullYear();
+  const startMonth = format(startDate, "M");
+  const endMonth = format(monthEnd, "M");
 
-    const client = await connectDB;
-    const db = client.db('menber');
+  const client = await connectDB;
+  const db = client.db("menber");
 
-    // let delet = await db.collection('daliy').deleteMany({
-    //     email: session.user.email,
-    //     month: { $eq: `${format(currentMonth, 'M') - 2}` },
-    // });
-    let result = await db
-        .collection('daliy')
-        .find({
-            email: session.user.email,
-            year: `${year}`,
-            month: { $gte: startMonth, $lte: endMonth },
-        })
-        .toArray();
+  let result = await db
+    .collection("daliy")
+    .find({
+      email: session.user.email,
+      year: `${year}`,
+      month: { $gte: startMonth, $lte: endMonth },
+    })
+    .toArray();
 
-    result = result.map((a) => {
-        a._id = a._id.toString();
-        return a;
-    });
+  result = result.map((a) => {
+    a._id = a._id.toString();
+    return a;
+  });
 
-    return <><Calender session={session} record={result} /></>;
+  return (
+    <div>
+      <div className={classes.header}>
+        <span>
+          {format(currentMonth, "yyy")}년 -
+          <span> {format(currentMonth, "M")}월</span>
+        </span>
+      </div>
+      <div className={classes.days}>
+        {date.map((date, idx) => {
+          return (
+            <div className={classes.day} key={idx}>
+              {date}
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <CalenderCell
+          session={session}
+          currentMonth={currentMonth}
+          checkDate={result}
+        />
+      </div>
+    </div>
+  );
 }
