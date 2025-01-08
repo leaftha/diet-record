4aea09a1c9e40b0329397eff6eea7322bf1cfa04
SaChan <vltpcks@gmail.com>
2025-01-08 21:13:16 +0900

    목표체중 변경 구조 및 css 수정

--- app/profileSetup/SetupForm.js
+++ app/profileSetup/SetupForm.js
@@ -1,37 +1,54 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import classes from './SetupForm.module.css';
+import { useState } from "react";
+import classes from "./SetupForm.module.css";
 
 export default function Setup({ sessionEmail, currentWeight, currentInbody }) {
-    const [weight, setWeight] = useState(currentWeight ? currentWeight : 0);
-    const [inbody, setInbody] = useState(currentInbody ? currentInbody : 0);
+  const [weight, setWeight] = useState(currentWeight ? currentWeight : 0);
+  const [inbody, setInbody] = useState(currentInbody ? currentInbody : 0);
 
-    const weightChange = (e) => {
-        setWeight(e.target.value);
-    };
-    const inbodyChange = (e) => {
-        setInbody(e.target.value);
-    };
+  const weightChange = (e) => {
+    setWeight(e.target.value);
+  };
+  const inbodyChange = (e) => {
+    setInbody(e.target.value);
+  };
 
-    const email = sessionEmail;
+  const email = sessionEmail;
 
-    return (
-        <div className={classes.main}>
-            <form className={classes.form} method="POST" action="/api/post/profile">
-                <div className={classes.item}>
-                    <label>목표 체중 변경</label>
-                    <input type="number" name="weight" onChange={weightChange} value={weight} min={0} />
-                </div>
-                <div className={classes.item}>
-                    <label>목표 체지방 변경</label>
-                    <input type="number" name="inbody" onChange={inbodyChange} value={inbody} min={0} />
-                </div>
-                <input className={classes.email} type="text" name="email" defaultValue={email} />
-                <button className={classes.btn} type="submit">
-                    Submit
-                </button>
-            </form>
+  return (
+    <>
+      <form className={classes.form} method="POST" action="/api/post/profile">
+        <div className={classes.item}>
+          <label>목표 체중 변경</label>
+          <input
+            type="number"
+            name="weight"
+            onChange={weightChange}
+            value={weight}
+            min={0}
+          />
         </div>
-    );
+        <div className={classes.item}>
+          <label>목표 체지방 변경</label>
+          <input
+            type="number"
+            name="inbody"
+            onChange={inbodyChange}
+            value={inbody}
+            min={0}
+          />
+        </div>
+        <input
+          className={classes.email}
+          type="text"
+          name="email"
+          defaultValue={email}
+        />
+        <button className={classes.btn} type="submit">
+          변경
+        </button>
+      </form>
+    </>
+  );
 }
--- app/profileSetup/page.js
+++ app/profileSetup/page.js
@@ -1,33 +1,46 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth].js';
-import { getServerSession } from 'next-auth';
-import { connectDB } from '@/util/database';
-import NotAuth from '../notauth';
-import SetupForm from './SetupForm';
-import classes from './page.module.css';
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import { connectDB } from "@/util/database";
+import NotAuth from "../notauth";
+import SetupForm from "./SetupForm";
+import classes from "./page.module.css";
 
 export default async function InBody() {
-    let session = await getServerSession(authOptions);
-    if (!session) {
-        return NotAuth();
-    }
-    const client = await connectDB;
-    const db = client.db('menber');
+  let session = await getServerSession(authOptions);
+  if (!session) {
+    return NotAuth();
+  }
+  const client = await connectDB;
+  const db = client.db("menber");
 
-    let result = await db
-        .collection('user_cred')
-        .find({ email: session.user.email })
-        .sort({ _id: -1 })
-        .limit(12)
-        .toArray();
+  let result = await db
+    .collection("user_cred")
+    .find({ email: session.user.email })
+    .sort({ _id: -1 })
+    .limit(12)
+    .toArray();
 
-    let currentWeight = result[0].weight;
-    let currentInbody = result[0].inbody;
+  let currentWeight = result[0].weight;
+  let currentInbody = result[0].inbody;
 
-    return (
-        <div className={classes.main}>
-            <p className={classes.item}>현재 목표 체중 {currentWeight ? currentWeight : '목표 설정 X'}kg</p>
-            <p className={classes.item}>현재 목표 체지방 {currentInbody ? currentInbody : '목표 설정 X'}%</p>
-            <SetupForm sessionEmail={session.user.email} currentWeight={currentWeight} currentInbody={currentInbody} />
+  return (
+    <div className={classes.main}>
+      <h1>목표 체중 변경</h1>
+      <div className={classes.body}>
+        <div>
+          <p className={classes.item}>
+            현재 목표 체중 : {currentWeight ? currentWeight : "목표 설정 X"}kg
+          </p>
+          <p className={classes.item}>
+            현재 목표 체지방 : {currentInbody ? currentInbody : "목표 설정 X"}%
+          </p>
         </div>
-    );
+        <SetupForm
+          sessionEmail={session.user.email}
+          currentWeight={currentWeight}
+          currentInbody={currentInbody}
+        />
+      </div>
+    </div>
+  );
 }
