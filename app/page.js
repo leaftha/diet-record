e1628863fa75e5f2bd129c80d41c52b26dbe141d
SaChan <vltpcks@gmail.com>
2025-01-19 20:03:51 +0900

    navbar를 위한 padding조절

--- app/page.js
+++ app/page.js
@@ -7,12 +7,18 @@ const Home = () => {
         <img src="/images/main2.jpg" alt="main img"></img>
         <ul className={classes.desc}>
           <div className={classes.descContent}>
-            <li>다이어트를 위한 사이트입니다.</li>
+            <li>
+              <span className={classes.stress}>다이어트</span>를 위한
+              사이트입니다.
+            </li>
             <li>자신의 체중, 근육량, 체지방등을 입력하여 비교할수 있습니다.</li>
             <li>
               하루 운동 기록을 입력하여 달력에 얼마나 운동했는지 알수 있습니다.
             </li>
-            <li>여기서 당신의 도전이 성공하길!</li>
+            <li>
+              여기서 당신의 <span className={classes.stress}>도전</span>이
+              성공하길!
+            </li>
           </div>
         </ul>
       </div>
