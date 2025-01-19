1d2122b1a02f08b1a0fa7c3073ae14f410cfa799
SaChan <vltpcks@gmail.com>
2025-01-19 21:22:01 +0900

    사이트 소개 카드 추가

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className={classes.content}>
       <div className={classes.body}>
-        <img src="/images/main2.jpg" alt="main img"></img>
+        <img src="/images/main2.jpg" alt="main img" />
         <ul className={classes.desc}>
           <div className={classes.descContent}>
             <li>
@@ -22,8 +22,25 @@ const Home = () => {
           </div>
         </ul>
       </div>
-      <div>
-        <h1>인바디</h1>
+      <div className={classes.main}>
+        <div className={classes.cardList}>
+          <div className={classes.card}>
+            <div className={classes.cardBody}>
+              <h1>인바디</h1>
+              <p>
+                자신의 인바디와 체중을 달마다 기록하여 그래프로 비교를 해보세요.
+              </p>
+              <p>
+                옆의 체중 입력 버튼을 눌러 이번달의
+                체중,근육량,체지방,체지방률을 입력할 수 있습니다.
+              </p>
+              <p>그래프를 통해 이전 달들과 비교를 할 수 있습니다.</p>
+            </div>
+          </div>
+          <div className={classes.card}>
+            <img src="/images/inbody.jpg" alt="inbody img" />
+          </div>
+        </div>
       </div>
     </div>
   );
