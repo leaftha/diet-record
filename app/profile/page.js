08955db352e6401f8bf6f937d7ade3ab9ab3936a
SaChan <vltpcks@gmail.com>
2025-01-20 20:50:07 +0900

    개인 페이지 디자인 수정

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,78 +1,76 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth].js';
-import { getServerSession } from 'next-auth';
-import { connectDB } from '@/util/database';
-import NotAuth from '../notauth';
-import Link from 'next/link';
-import classes from './page.module.css';
-import Widhdrawal from './withdrawal';
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import { connectDB } from "@/util/database";
+import NotAuth from "../notauth";
+import Link from "next/link";
+import classes from "./page.module.css";
+import Widhdrawal from "./withdrawal";
 
 export default async function InBody() {
-    let session = await getServerSession(authOptions);
-    if (session === null) {
-        return NotAuth();
-    }
-    const client = await connectDB;
-    const db = client.db('menber');
+  let session = await getServerSession(authOptions);
+  if (session === null) {
+    return NotAuth();
+  }
+  const client = await connectDB;
+  const db = client.db("menber");
 
-    let result = await db
-        .collection('inbody')
-        .find({ email: session.user.email })
-        .sort({ _id: -1 })
-        .limit(12)
-        .toArray();
+  let result = await db
+    .collection("inbody")
+    .find({ email: session.user.email })
+    .sort({ _id: -1 })
+    .limit(12)
+    .toArray();
 
-    let Goal = await db
-        .collection('user_cred')
-        .find({ email: session.user.email })
-        .sort({ _id: -1 })
-        .limit(12)
-        .toArray();
+  let Goal = await db
+    .collection("user_cred")
+    .find({ email: session.user.email })
+    .sort({ _id: -1 })
+    .limit(12)
+    .toArray();
 
-    result = result.map((a) => {
-        a._id = a._id.toString();
-        return a;
-    });
-    let currentWeight;
-    let currentInbody;
-    if (result.length != 0) {
-        currentWeight = result[0].weight;
-        currentInbody = result[0].fatper;
-    } else {
-        currentWeight = '--';
-        currentInbody = '--';
-    }
-    const goalWeight = Goal[0].weight ? Goal[0].weight : '--';
-    const goalInbody = Goal[0].inbody ? Goal[0].inbody : '--';
+  result = result.map((a) => {
+    a._id = a._id.toString();
+    return a;
+  });
+  let currentWeight;
+  let currentInbody;
+  if (result.length != 0) {
+    currentWeight = result[0].weight;
+    currentInbody = result[0].fatper;
+  } else {
+    currentWeight = "--";
+    currentInbody = "--";
+  }
+  const goalWeight = Goal[0].weight ? Goal[0].weight : "--";
+  const goalInbody = Goal[0].inbody ? Goal[0].inbody : "--";
 
-    return (
-        <div className={classes.container}>
-            <h1 className={classes.title}>회원 정보</h1>
-            <div className={classes.body}>
-                <div className={classes.main}>
-                    <div className={classes.item}>
-                        <p>이름 : {session.user.name}</p>
-                        <p>이메일 : {session.user.email}</p>
-                    </div>
-
-                    <div className={classes.item}>
-                        <p>목표 체중 : {goalWeight}kg</p>
-                        <p>목표 체지방률 : {goalInbody}%</p>
-                    </div>
-
-                    <div className={classes.item}>
-                        <p>현재 체중 : {currentWeight}kg</p>
-                        <p>현재 체지방률 : {currentInbody}%</p>
-                    </div>
-                </div>
-                <div className={classes.list}>
-                    <div className={classes.change}>
-                        <Link className={classes.link} href="profileSetup">
-                            목표 체중 설정
-                        </Link>
-                    </div>
-                    <Widhdrawal session={session} />
-                </div>
-            </div>
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.title}>회원 정보</h1>
+      <div className={classes.body}>
+        <div className={classes.list}>
+          <Link className={classes.btn} href="profileSetup">
+            목표 체중 설정
+          </Link>
+          <Widhdrawal session={session} />
         </div>
-    );
+        <div className={classes.main}>
+          <div className={classes.item}>
+            <p>이름 : {session.user.name}</p>
+            <p>이메일 : {session.user.email}</p>
+          </div>
+
+          <div className={classes.item}>
+            <p>목표 체중 : {goalWeight}kg</p>
+            <p>목표 체지방률 : {goalInbody}%</p>
+          </div>
+
+          <div className={classes.item}>
+            <p>현재 체중 : {currentWeight}kg</p>
+            <p>현재 체지방률 : {currentInbody}%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/profile/withdrawal.js
+++ app/profile/withdrawal.js
@@ -1,24 +1,22 @@
-'use client';
+"use client";
 
-import Modal from './modal';
-import classes from './withdrawal.module.css';
-import { useState } from 'react';
+import Modal from "./modal";
+import classes from "./withdrawal.module.css";
+import { useState } from "react";
 
 export default function Widhdrawal({ session }) {
-    const [modal, setModal] = useState(false);
-    return (
-        <div>
-            <div className={classes.main}>
-                <button
-                    onClick={() => {
-                        setModal(true);
-                    }}
-                    className={classes.btn}
-                >
-                    회원탈퇴
-                </button>
-            </div>
-            {modal && <Modal session={session} setModal={setModal} />}
-        </div>
-    );
+  const [modal, setModal] = useState(false);
+  return (
+    <>
+      <p
+        onClick={() => {
+          setModal(true);
+        }}
+        className={classes.btn}
+      >
+        회원탈퇴
+      </p>
+      {modal && <Modal session={session} setModal={setModal} />}
+    </>
+  );
 }
