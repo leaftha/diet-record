25586207b4f1d456ff59e286a14fe2fb342d15c5
leaftha <vltpcks@gmail.com>
2025-01-20 10:17:46 +0900

    메인 페이지 동영상으로 교체

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Link from 'next/link';
-import LoginBtn from './loginbtn';
 import classes from './page.module.css';
-import { signIn } from 'next-auth/react';
 
 const Home = () => {
     return (
         <div className={classes.content}>
             <div className={classes.body}>
-                <img src="/images/main2.jpg" alt="main img" />
+                <video width="100%" muted loop autoPlay>
+                    <source src="/running_video.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                {/* <img src="/images/main2.jpg" alt="main img" /> */}
                 <div className={classes.btns}>
                     <div className={classes.des}>
                         <h1>Diet Web</h1>
@@ -17,18 +19,6 @@ const Home = () => {
                         회원가입
                     </Link>
                 </div>
-                {/* <ul className={classes.desc}>
-                    <div className={classes.descContent}>
-                        <li>
-                            <span className={classes.stress}>다이어트</span>를 위한 사이트입니다.
-                        </li>
-                        <li>자신의 체중, 근육량, 체지방등을 입력하여 비교할수 있습니다.</li>
-                        <li>하루 운동 기록을 입력하여 달력에 얼마나 운동했는지 알수 있습니다.</li>
-                        <li>
-                            여기서 당신의 <span className={classes.stress}>도전</span>이 성공하길!
-                        </li>
-                    </div>
-                </ul> */}
             </div>
         </div>
     );
