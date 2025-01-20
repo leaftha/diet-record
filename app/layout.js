db17495338c2132b458e16b74186ad500f0073f4
leaftha <vltpcks@gmail.com>
2025-01-20 10:09:17 +0900

    메인 페이지 디자인 수정

--- app/layout.js
+++ app/layout.js
@@ -1,51 +1,47 @@
-import "./globals.css";
-import LoginBtn from "./loginbtn";
-import Link from "next/link";
-import { authOptions } from "@/pages/api/auth/[...nextauth].js";
-import { getServerSession } from "next-auth";
-import LogoutBtn from "./logoutbtn";
-
+import './globals.css';
+import LoginBtn from './loginbtn';
+import Link from 'next/link';
+import { authOptions } from '@/pages/api/auth/[...nextauth].js';
+import { getServerSession } from 'next-auth';
+import LogoutBtn from './logoutbtn';
 
 export const metadata = {
-  title: "diet record",
-  description: "diet to record",
+    title: 'diet record',
+    description: 'diet to record',
 };
 
 export default async function RootLayout({ children }) {
-  let session = await getServerSession(authOptions);
-  return (
-    <html lang="ko">
-      <body suppressHydrationWarning={true}>
-        <nav className="navbar">
-          <div className="navbar-content">
-            <Link href="/" className="navbar-name">
-              Diet
-            </Link>
-            <Link href="inbody" className="inbody">
-              인바디
-            </Link>
-            <Link href="daily" className="daily">
-              운동 기록
-            </Link>
-          </div>
-          {session === null ? (
-            <div className="navbar-auth">
-              <LoginBtn />
-              <Link className="register" href="register">
-                회원가입
-              </Link>
-            </div>
-          ) : (
-            <div className="navbar-logined">
-              <Link href="profile" className="profile">
-                프로필
-              </Link>
-              <LogoutBtn />
-            </div>
-          )}
-        </nav>
-        {children}
-      </body>
-    </html>
-  );
+    let session = await getServerSession(authOptions);
+    return (
+        <html lang="ko">
+            <body suppressHydrationWarning={true}>
+                <nav className="navbar">
+                    <div className="navbar-content">
+                        <Link href="/" className="navbar-name">
+                            Diet
+                        </Link>
+                        <Link href="inbody" className="inbody">
+                            인바디
+                        </Link>
+                        <Link href="daily" className="daily">
+                            운동 기록
+                        </Link>
+                    </div>
+                    {session === null ? (
+                        <div className="navbar-auth">
+                            <LoginBtn />
+                        </div>
+                    ) : (
+                        <div className="navbar-logined">
+                            <Link href="profile" className="profile">
+                                프로필
+                            </Link>
+                            <LogoutBtn />
+                        </div>
+                    )}
+                </nav>
+                {children}
+            </body>
+        </html>
+    );
 }
--- app/page.js
+++ app/page.js
@@ -1,49 +1,37 @@
-import classes from "./page.module.css";
+import Link from 'next/link';
+import LoginBtn from './loginbtn';
+import classes from './page.module.css';
+import { signIn } from 'next-auth/react';
 
 const Home = () => {
-  return (
-    <div className={classes.content}>
-      <div className={classes.body}>
-        <img src="/images/main2.jpg" alt="main img" />
-        <ul className={classes.desc}>
-          <div className={classes.descContent}>
-            <li>
-              <span className={classes.stress}>다이어트</span>를 위한
-              사이트입니다.
-            </li>
-            <li>자신의 체중, 근육량, 체지방등을 입력하여 비교할수 있습니다.</li>
-            <li>
-              하루 운동 기록을 입력하여 달력에 얼마나 운동했는지 알수 있습니다.
-            </li>
-            <li>
-              여기서 당신의 <span className={classes.stress}>도전</span>이
-              성공하길!
-            </li>
-          </div>
-        </ul>
-      </div>
-      <div className={classes.main}>
-        <div className={classes.cardList}>
-          <div className={classes.card}>
-            <div className={classes.cardBody}>
-              <h1>인바디</h1>
-              <p>
-                자신의 인바디와 체중을 달마다 기록하여 그래프로 비교를 해보세요.
-              </p>
-              <p>
-                옆의 체중 입력 버튼을 눌러 이번달의
-                체중,근육량,체지방,체지방률을 입력할 수 있습니다.
-              </p>
-              <p>그래프를 통해 이전 달들과 비교를 할 수 있습니다.</p>
+    return (
+        <div className={classes.content}>
+            <div className={classes.body}>
+                <img src="/images/main2.jpg" alt="main img" />
+                <div className={classes.btns}>
+                    <div className={classes.des}>
+                        <h1>Diet Web</h1>
+                        <p>여기서 당신의 도전을 성공시켜 보세요!</p>
+                    </div>
+                    <Link className={classes.btn} href="register">
+                        회원가입
+                    </Link>
+                </div>
+                {/* <ul className={classes.desc}>
+                    <div className={classes.descContent}>
+                        <li>
+                            <span className={classes.stress}>다이어트</span>를 위한 사이트입니다.
+                        </li>
+                        <li>자신의 체중, 근육량, 체지방등을 입력하여 비교할수 있습니다.</li>
+                        <li>하루 운동 기록을 입력하여 달력에 얼마나 운동했는지 알수 있습니다.</li>
+                        <li>
+                            여기서 당신의 <span className={classes.stress}>도전</span>이 성공하길!
+                        </li>
+                    </div>
+                </ul> */}
             </div>
-          </div>
-          <div className={classes.card}>
-            <img src="/images/inbody.jpg" alt="inbody img" />
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
