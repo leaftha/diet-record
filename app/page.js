da036d64754ecea933fc645ebc1762b770fe16c8
SaChan <vltpcks@gmail.com>
2025-01-20 22:05:53 +0900

    실수 수정

--- app/page.js
+++ app/page.js
@@ -1,27 +1,33 @@
-import Link from 'next/link';
-import classes from './page.module.css';
+import Link from "next/link";
+import { authOptions } from "@/pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+import classes from "./page.module.css";
 
-const Home = () => {
-    return (
-        <div className={classes.content}>
-            <div className={classes.body}>
-                <video width="100%" muted loop autoPlay>
-                    <source src="/running_video.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                {/* <img src="/images/main2.jpg" alt="main img" /> */}
-                <div className={classes.btns}>
-                    <div className={classes.des}>
-                        <h1>Diet Record</h1>
-                        <p>여기서 당신의 도전을 성공시켜 보세요!</p>
-                    </div>
-                    <Link className={classes.btn} href="register">
-                        회원가입
-                    </Link>
-                </div>
-            </div>
+const Home = async () => {
+  let session = await getServerSession(authOptions);
+
+  return (
+    <div className={classes.content}>
+      <div className={classes.body}>
+        <video width="100%" muted loop autoPlay>
+          <source src="/running_video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        {/* <img src="/images/main2.jpg" alt="main img" /> */}
+        <div className={classes.btns}>
+          <div className={classes.des}>
+            <h1>Diet Record</h1>
+            <p>여기서 당신의 도전을 성공시켜 보세요!</p>
+          </div>
+          {session === null && (
+            <Link className={classes.btn} href="register">
+              회원가입
+            </Link>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
