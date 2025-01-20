f31cdc1f720e7b7d209bfb4f08fed343ad43f005
leaftha <vltpcks@gmail.com>
2025-01-20 14:42:50 +0900

    모달 디자인 오류 수정

--- app/inbody/Modal.js
+++ app/inbody/Modal.js
@@ -1,40 +1,42 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import InputForm from "./inputform";
-import classes from "./Modal.module.css";
+import { useState } from 'react';
+import InputForm from './inputform';
+import classes from './Modal.module.css';
+import { createPortal } from 'react-dom';
 
 const Modal = ({ session }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return (
-    <div className={classes.body}>
-      <button
-        className={classes.btn}
-        onClick={() => {
-          setIsModalOpen(true);
-        }}
-      >
-        체중 입력
-      </button>
-      {isModalOpen && (
-        <ModalContent session={session} setIsModalOpen={setIsModalOpen} />
-      )}
-    </div>
-  );
+    return (
+        <>
+            <div className={classes.body}>
+                <button
+                    className={classes.btn}
+                    onClick={() => {
+                        setIsModalOpen(true);
+                    }}
+                >
+                    체중 입력
+                </button>
+            </div>
+            {isModalOpen && <ModalContent session={session} setIsModalOpen={setIsModalOpen} />}
+        </>
+    );
 };
 
 const ModalContent = ({ session, setIsModalOpen }) => {
-  return (
-    <div
-      className={classes.modal}
-      onClick={() => {
-        setIsModalOpen(false);
-      }}
-    >
-      <InputForm session={session} />
-    </div>
-  );
+    return createPortal(
+        <div
+            className={classes.modal}
+            onClick={() => {
+                setIsModalOpen(false);
+            }}
+        >
+            <InputForm session={session} />
+        </div>,
+        document.body
+    );
 };
 
 export default Modal;
